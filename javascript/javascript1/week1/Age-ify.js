a66baef48d0e5816214163e7bdd78ca302400201
Yaser-Aldammad <yaser_dammad@hotmail.com>
2020-05-28 16:55:21 +0200

    added new file A dog age calculator

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -5,4 +5,4 @@ let yearFuture =2027;
 let age=yearFuture - yearOfBirth;
 let string1 = "You will be ";
 let string2 =" years old in ";
-console.log(string1 + age + string2 + yearFuture);
+console.log(string1 + age + string2 + yearFuture); 
---
+++ javascript/javascript1/week1/Goodboy.js
@@ -0,0 +1,15 @@
+//Goodboy-Oldboy (A dog age calculator)
+
+let dogYearOfBirth ;
+let dogYearFuture ;
+let dogYear ;
+let shouldShowResultInDogYears =6; 
+if( shouldShowResultInDogYears == true){
+    dogYear=  dogYearFuture - dogYearOfBirth;
+    console.log("Your dog will be 70 dog years old in 2027")
+
+}else{
+    dogYear=  dogYearFuture - dogYearOfBirth +15;
+    console.log("Your dog will be 10 human years old in 2027")
+}
+
