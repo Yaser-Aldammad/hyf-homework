ffddf3cdc6430a88879eb05cd8dd01b2a5402976
Yaser-Aldammad <yaser_dammad@hotmail.com>
2020-07-10 23:36:25 +0200

    added types of variables

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -5,4 +5,7 @@ let yearFuture =2027;
 let age=yearFuture - yearOfBirth;
 let string1 = "You will be ";
 let string2 =" years old in ";
+let typeofBirth= typeof yearOfBirth;
+
+console.log(typeofBirth)
 console.log(string1 + age + string2 + yearFuture); 
