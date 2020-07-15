1b3f2d41bfb126e5bca1c6ee5b332745c1ce756d
Yaser-Aldammad <62471888+Yaser-Aldammad@users.noreply.github.com>
2020-07-15 23:24:02 +0200

    Update Age-ify.js

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -2,6 +2,8 @@
 
 let yearOfBirth =1983 ;
 let yearFuture =2027;
+let typeofyearOfBirth= typeof yearOfBirth;
+console.log(typeofyearOfBirth);
 let age=yearFuture - yearOfBirth;
 let string1 = "You will be ";
 let string2 =" years old in ";
