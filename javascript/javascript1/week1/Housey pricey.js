baf56046536d7d7ff2cd9fa606b8d7df978e382c
Yaser-Aldammad <62471888+Yaser-Aldammad@users.noreply.github.com>
2020-07-16 00:58:43 +0200

    Update Housey pricey.js

--- javascript/javascript1/week1/Housey pricey.js
+++ javascript/javascript1/week1/Housey pricey.js
@@ -1,32 +1,33 @@
 //Housey pricey (A house price estimator)
 
-let huseHeight = 10 ;
-let huseDepth = 10 ;
-let husWidth = 8 ;
-let gardenSizeInM2 = 100 ;
-let volumeInMeters = husWidth * huseDepth * huseHeight;
-let requstedPrice =  2500000;
+let Height = 10;
+let Depth = 10;
+let Width = 8;
+let gardenSizeInM2 = 100;
+const volumeInMeters = Width * Depth * Height;
+const peterhoursePrice =  2500000;
 housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
-if(housePrice  == requstedPrice){
-    console.log( "Good price, as it is expected!")
+if(housePrice == peterhoursePrice){
+    console.log( "Good price, as it is expected!");
 }
- else if (housePrice < requstedPrice) {
-    console.log("Cheap prise")
+ else if (housePrice < peterhoursePrice) {
+    console.log("Cheap prise");
 } else {
-    console.log("too much")
+    console.log("too much");
 }
- huseHeight = 8 ; 
-huseDepth = 11 ;
-husWidth = 5 ;
-gardenSizeInM2 = 70 ;
 
- requstedPrice =  1000000;
+Height= 8; 
+Depth= 11;
+Width= 5;
+gardenSizeInM2= 70;
 
-if(housePrice  == requstedPrice){
-    console.log( "Good price, as it is expected!")
+const juliahouseprice= 1000000;
+
+if(housePrice== juliahouseprice){
+    console.log( "Good price, as it is expected!");
 }
- else if (housePrice < requstedPrice) {
-    console.log("Cheap prise")
+ else if (housePrice < juliahouseprice) {
+    console.log("Cheap prise");
 } else {
-    console.log("too much")
+    console.log("too much");
 }
