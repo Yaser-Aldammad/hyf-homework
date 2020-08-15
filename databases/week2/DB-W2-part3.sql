1fd8cadea111ff1f091f1b3b0b8261a0fdf394a0
Yaser-Aldammad <yaser_dammad@hotmail.com>
2020-08-15 17:48:37 +0200

    DB-W2-part3

--- databases/week2/DB-W2-part3.sql
+++ databases/week2/DB-W2-part3.sql
@@ -3,34 +3,24 @@
 USE lesson_db;
 
 -- tasks assigned to useres => emails = @spotify.com 
-SELECT t.title
-FROM user_task ut 
-INNER JOIN task t ON ut.task_id=t.id 
-INNER JOIN (
-    SELECT u.id, u.email
-    FROM user u
-    WHERE email LIKE '%@spotify.com'
-) u ON ut.user_id=u.id;
+SELECT * from user
+WHERE email like '%@spotify.com';
 
 -- tasks for Donald Duck
-SELECT DISTINCT user.name, task.id, task.title, task.description, status.name FROM user
-INNER JOIN user_task ON user.id = user_task.task_id
-INNER JOIN task ON task.id = user_task.task_id
-INNER JOIN status ON task.status_id = status_id
-WHERE user.name LIKE '%Donald Duck%' AND status.name = 'Not started';
+SELECT  task.* from task
+join user on user.name ="Donald Duck" and user.id = task.user_id
+join status on status.name ='Not started' and status.id = task.status_id;
 
 -- tasks for Maryrose Meadows
-SELECT * FROM user
-JOIN user_task ON user.id = user_task.user_id 
-JOIN task ON user_task.task_id = task.id 
-WHERE user.name = 'Aubry Polak' AND MONTH(created) = 05;
+SELECT  task.* from task
 
--- use group by
-SELECT 
-count(*), MONTHNAME(created) 
-AS 
-    task_per_month 
-FROM task GROUP BY task_per_month;
+join user on user.name ="Maryrose Meadows" and user.id = task.user_id
+WHERE month(task.created) = 9;
+ SELECT  month(created) month ,
+ COUNT(*) as tasks
+ FROM task
+GROUP BY month(created);
+
 
 
 
