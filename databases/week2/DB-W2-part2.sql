fc7242029f9569702c0439651c529b5b04dba92e
Yaser-Aldammad <yaser_dammad@hotmail.com>
2020-08-18 11:21:25 +0200

    revision modified

--- databases/week2/DB-W2-part2.sql
+++ databases/week2/DB-W2-part2.sql
@@ -2,20 +2,27 @@
 CREATE DATABASE DBpart2;
 USE DBpart2;
 CREATE TABLE class (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(50) NOT NULL,
-begins DATETIME,
-ends DATETIME
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50) NOT NULL,
+`begins` DATETIME,
+`ends` DATETIME
 );
 
 -- part 2, school database
 CREATE TABLE student (
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(50) NOT NULL,
-email VARCHAR(50) NOT NULL,
-phone INT NOT NULL,
-class_id INT NOT NULL,
-FOREIGN KEY (class_id) REFERENCES class(id)
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50) NOT NULL,
+`email` VARCHAR(50) NOT NULL,
+`phone` INT NOT NULL
+);
+
+-- relational table
+CREATE TABLE class_students (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`class_id` INT NOT NULL,
+`student_id` INT NOT NULL,
+FOREIGN KEY (class_id) REFERENCES class(id),
+FOREIGN KEY (student_id) REFERENCES student(id)
 );
 
 -- create an index
@@ -24,4 +31,4 @@ ON student(name);
 
 -- adding new column
 ALTER TABLE class
-ADD COLUMN status ENUM('not started', 'ongoing', 'finished');
+ADD COLUMN status ENUM('not started', 'ongoing', 'finished') default('ongoing');
