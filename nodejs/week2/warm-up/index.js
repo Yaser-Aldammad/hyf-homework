9e79a2dc15d703775685bb2272983f440cf4e235
Yaser-Aldammad <62471888+Yaser-Aldammad@users.noreply.github.com>
2020-10-13 00:44:45 +0200

    Update index.js

--- nodejs/week2/warm-up/index.js
+++ nodejs/week2/warm-up/index.js
@@ -1,25 +1,16 @@
 const express = require("express");
-const { response, request } = require("express");
 const app = express();
 
-app.get("/", (request, respose) => {
-  respose.send("<h1>Worm-up exercise</h1>");
-});
-app.get("/number/add", async (request, response) => {
-  let number_1 = request.query.first;
-  let number_2 = request.query.second;
-
-  let result = number_1 + number_2;
-  response.send({ result });
+app.get("/numbers/add", async (request, response) => {
+  let result = Number(request.query.first) + Number(request.query.second);
+  response.send(`the result is ${result}`);
 });
 
-app.get("/number/multiply", async (request, response) => {
-  let number_1 = request.query.first;
-  let number_2 = request.query.second;
-
-  let result = number_1 * number_2;
-
-  response.send({ result });
+app.get("/numbers/multiply/:first/:second", async (request, response) => {
+  let result = Number(request.params.first) * Number(request.params.second);
+  response.send(`the result is ${result}`);
 });
-app.listen(3000);
-console.log("server runing on port 3000");
+
+app.listen(3000, () => {
+  console.log(` listening on port 3000!`);
+});
